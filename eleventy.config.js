0b4e8f33330f8b6be6e5ec6b1cc6eb9bfa639d70
honeybody <honeybody@protonmail.com>
2025-12-09 15:49:34 -0600

    first commit

---
+++ eleventy.config.js
@@ -0,0 +1,59 @@
+import format from "date-fns/format";
+import htmlmin from "html-minifier";
+import markdownIt from "markdown-it";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import * as toc from "eleventy-plugin-toc-util";
+
+const md = markdownIt();
+
+export default async function (eleventyConfig) {
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+
+  eleventyConfig.addPassthroughCopy("assets");
+
+  // -------------------------------------------------------------------------------------------------------------------------------------
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+
+  // -------------------------------------------------------------------------------------------------------------------------------------
+
+  eleventyConfig.addFilter("slice", (array, number) => {
+    return array.slice(0, number);
+  });
+
+  eleventyConfig.addFilter("unquote", (string) => {
+    return string.slice(0, -1).substring(1);
+  });
+
+  eleventyConfig.addFilter("date", function (date, dateFormat) {
+    return format(date, dateFormat);
+  });
+
+  eleventyConfig.addFilter("markdownify", function (content) {
+    return md.render(String(content));
+  });
+
+  eleventyConfig.addFilter("attachId", (html) => toc.attachId(html, "h2"));
+  eleventyConfig.addFilter("attachAnchor", (html) => toc.attachIdAnchor(html, "h2", ""));
+  eleventyConfig.addFilter("toc", (html) => toc.createToc(html, "h2"));
+
+  // -------------------------------------------------------------------------------------------------------------------------------------
+
+  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,
+        useShortDoctype: true,
+      });
+    }
+
+    return content;
+  });
+}
+
+export const config = {
+  markdownTemplateEngine: "njk",
+};
